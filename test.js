bbbd2a76c566b9116dda9ea8c99fac37b77fb21f
jackypark9852 <jackypark9852@gmail.com>
2023-01-04 00:32:37 -0600

    Update tweet text generation function

--- test.js
+++ test.js
@@ -15,3 +15,13 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+const tweets = await GetElonTweets();
+console.log(tweets);
+const text = await GenerateTweetText(tweets[4].text);
+console.log(text);
+
+// const prompt =
+//   "I just created an image inspired by @elonmusk's latest tweet - highlighting that art can help us interpret world events. US govt's suspension of 250k accounts is an important one! #freedomofexpression #artsignificance https://t.co/Yxxxxx";
+// const link_removed = prompt
+// console.log(link_removed);
